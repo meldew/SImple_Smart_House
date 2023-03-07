47c2a34c9d5015bcd993409775980619c30c3f4d
Mikael Walde <kirillgja@gmail.com>
2023-03-07 12:51:26 +0100

    bla

--- random Testfiles/testList.py
+++ random Testfiles/testList.py
@@ -1,21 +1,27 @@
-liste = []
-caller_list = []
+# liste = []
+# caller_list = []
 
-class test():
-    def __init__(self, name, serial):
-        self.name = str(name)
-        self.serial = int(serial)
-        self.func()
+# class test():
+#     def __init__(self, name, serial):
+#         self.name = str(name)
+#         self.serial = int(serial)
+#         self.func()
 
-    def func(self):
-        newtuple = (self.name, self.serial)
-        liste.append(newtuple)
+#     def func(self):
+#         newtuple = (self.name, self.serial)
+#         liste.append(newtuple)
 
 
-newobject = test("testobjekt", 12341234)
-newobject1 = test("testobjekt", 12341234)
-newobject2 = test("testobjekt", 12341234)
-newobject3 = test("testobjekt", 12341234)
+# newobject = test("testobjekt", 12341234)
+# newobject1 = test("testobjekt", 12341234)
+# newobject2 = test("testobjekt", 12341234)
+# newobject3 = test("testobjekt", 12341234)
 
-print(liste)
+# print(liste)
 
+dictionary = {
+    "Room1": [20.5, 30.5, 25.5],
+    "Room2": [20.5, 30.5, 25.6],
+}
+
+print(dictionary["Room1"][2])
