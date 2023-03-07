61e38fce09445a6309004b5d1eef2319a39607ad
Mikael Walde <kirillgja@gmail.com>
2023-03-07 15:20:39 +0100

    .

--- random Testfiles/pytohn_sql_test.py
+++ random Testfiles/pytohn_sql_test.py
@@ -1,40 +1,19 @@
-import pandas as pd
 from sqlite3 import Connection
 conn = Connection("db.sqlite")
 cursor = conn.cursor()
-# cursor.execute("SELECT device, AVG(value) AS avg_temp FROM measurements WHERE device IN (8, 12 ,28) GROUP BY device ORDER BY avg_temp ASC LIMIT 1;")
-# results = cursor.fetchall()
-# for result in results:
-#     sensor_id = result
-# cursor.execute("SELECT name FROM rooms WHERE id = (SELECT room FROM devices WHERE id ="+ str(sensor_id[0])+");")
-# results = cursor.fetchall()
-# for result in results:
-#     room_name = result
-def test():
-    device_8 = []
-    cursor.execute("SELECT value from measurements WHERE device = 8")
-    results = cursor.fetchall()
-    for result in results:
-        device_8.append(result[0])
-    s1 = pd.Series(device_8)
-   
-    device_12 = []
-    cursor.execute("SELECT value from measurements WHERE device = 12")
-    results = cursor.fetchall()
-    for result in results:
-        device_12.append(result[0])
-    s2 = pd.Series(device_8)
 
-    device_28 = []
-    cursor.execute("SELECT value from measurements WHERE device = 28")
-    results = cursor.fetchall()
-    for result in results:
-        device_28.append(result[0])
-    s3 = pd.Series(device_8)
-   
-    dictionary = {
-        "Room1": [s1.describe()[3], s1.describe()[7], s1.describe()[1]],
-        "Room2": [s2.describe()[3], s2.describe()[7], s2.describe()[1]],
-        "Room3": [s3.describe()[3], s3.describe()[7], s3.describe()[1]]
-    }
-    return dictionary
+list_with_values = []
+cursor.execute("""SELECT MAX(value), AVG(value), MIN(value) from measurements 
+WHERE device = 8 OR device = 12 OR device = 28
+GROUP by device
+""")
+results = cursor.fetchall()
+for result in results:
+  for x in result: 
+    list_with_values.append(x)
+print(list_with_values)
+dictionary = {
+    "Living Room / Kitchen": [list_with_values[5],list_with_values[3], list_with_values[4]],
+    "Entrance": [list_with_values[2],list_with_values[0], list_with_values[1]],
+    "Master Bedroom": [list_with_values[8],list_with_values[6], list_with_values[7]]
+}
