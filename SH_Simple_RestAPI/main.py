21544417e6c68fc44fe27863f20715f096aa969b
mrtor <585020@stud.hvl.no>
2023-03-28 17:02:05 +0200

    Lagt till funksjoner i main

--- SH_Simple_RestAPI/main.py
+++ SH_Simple_RestAPI/main.py
@@ -29,6 +29,97 @@ app.mount("/welcome", StaticFiles(directory="static"), name="static")
 def root():
     return {"message": "Welcome to SmartHouse Cloud REST API - Powered by FastAPI"}
 
+@app.get("/smarthouse/")
+def GetSmarthouse():
+    # Informasjon om smarthuset
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.get("/smarthouse/floor")
+def GetSmarthousefloor():
+    # Informasjon om alle etasjer
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.get("/smarthouse/floor/{fid}")
+def GetSmarthousefloorid():
+    # Informasjon om spesifikk etasje
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.get("/smarthouse/floor/{fid}/room")
+def GetSmarthouserooms():
+    # Informasjon om alle rom i en etasje
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.get("/smarthouse/floor/{fid}/room/{rid}")
+def GetSmarthouseroom():
+    # Informasjon om ett rom i en etasje
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.get("/smarthouse/device")
+def GetSmarthouseDevices():
+    # Informasjon om alle enheter
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.get("/smarthouse/device/{did}")
+def GetSmarthouseDeviceID():
+    # Informasjon om spesifikk enhet
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.get("/smarthouse/sensor/{did}/current")
+def GetSmarthouseDeviceStatus():
+    # Status på sensor (avgitt måleverdi)
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.post("/smarthouse/sensor/{did}/current")
+def PostSmartHouseSensorCurrent():
+    # Legg til måling for spesifisert enhet
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.get("/smarthouse/sensor/{did}/values?limit=n")
+def Getlatestmeasurement():
+    # Siste måling(er) for spesifikk sensor - antall målinger returnert gitt av "n"
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.delete("/smarthouse/sensor/{did}/oldest")
+def Deleteoldestmeasurement():
+    # Slett eldste måling fra gitt sensor
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.get("/smarthouse/actuator/{did}/current")
+def Getactuatorstatus():
+    # Hent status på aktuator
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
+@app.put("/smarthouse/device/{did}")
+def Updateactuator():
+    # Oppdater status på aktuator
+    # TODO Legg inn funksjon
+    return NotImplemented
+
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
